64488d66a34572a28ede92623785797aae5097c7
Rosangela <rosangela.cavalcante5@gmail.com>
2021-09-04 10:11:50 -0400

    introducao

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>{{ titulo }}</h1>
+
+  <input v-model="tarefa.descricao" type="text" placeholder="Descrição da tarefa">
+  <button @click="adicionarTarefa">Adicionar</button>
+
+  <table border="1">
+    <thead>
+      <th>Concluída</th>
+      <th>Descrição</th>
+    </thead>
+    <tbody>
+      <tr v-for="iterador in tarefas" :key="iterador.descricao" :class="{ riscado: iterador.concluida }">
+        <td>
+          <input type="checkbox" v-model="iterador.concluida">
+        </td>
+        <td>{{ iterador.descricao }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Todolist Vite",
+      tarefas: [],
+      tarefa: {
+        descricao: "",
+        concluida: false
+      }
+    }
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push(Object.assign({}, this.tarefa))
+    }
+  }
+}
+</script>
+
+<style>
+  .riscado {
+    text-decoration: line-through;
+  }
+</style>
