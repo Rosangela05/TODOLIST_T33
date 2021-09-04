a475d15a2433a88db74a16da633bafa695634fff
Rosangela <rosangela.cavalcante5@gmail.com>
2021-09-04 11:03:23 -0400

    Espaços corretos

--- src/components/AppBtn.vue
+++ src/components/AppBtn.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="h-12 bg-transparent border-white border-2 text-white px-8 rounded-full block">
+  <button class="h-12 bg-transparent border-white border-2 text-white px-8 rounded-full block ">
+
+
     <slot></slot>
   </button>
 </template>
--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="max-w-md mx-auto rounded-full flex items-center bg-white h-12  ">
 
-        
+
         <div class="w-full px-4">
+
+            
             <slot></slot>
         </div>
     </div>
